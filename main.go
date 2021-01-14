ed58ba8dd46e153c63e5cb527c374a452716b9e4
정설민 <soolmini@neowiz.com>
2021-01-14 15:46:11 +0900

    get ...string list and Print

--- main.go
+++ main.go
@@ -1,15 +1,11 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
+import "fmt"
 
-func lenAndUpper(name string) (int, string) {
-	return len(name), strings.ToUpper(name)
+func repeatMe(words ...string) {
+	fmt.Println(words)
 }
 
 func main() {
-	totalLength, _ := lenAndUpper("sul")
-	fmt.Println(totalLength)
+	repeatMe("sul", "min", "mon", "ths")
 }
