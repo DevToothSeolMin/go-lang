8534b1ddccd56a8dc2959e3438b2b6ccdf077081
정설민 <soolmini@neowiz.com>
2021-01-14 16:39:07 +0900

    Basic Pointer

--- main.go
+++ main.go
@@ -4,17 +4,9 @@ import (
 	"fmt"
 )
 
-func canDrink(age int) bool {
-	switch koreanAge := age + 2; koreanAge {
-	case 10:
-		return false
-	case 18:
-		return true
-	}
-
-	return false
-}
-
 func main() {
-	fmt.Println(canDrink(18))
+	a := 2
+	b := &a
+	*b = 10
+	fmt.Println(a)
 }
